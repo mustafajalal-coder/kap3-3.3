4b087be2addfcd0c52a8706cd8d470a5953b42c8
muja07001 <muja07001@E-PW05CGR9>
2024-11-18 13:01:00 +0100

    Add project files.

---
+++ kap3,3.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kap3_3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur många timmar vill du hyra ut bilen?");
+            int timmar = int.Parse(Console.ReadLine());
+
+            int kostnad = timmar * 80;
+
+            if (kostnad > 950)
+
+            {
+                kostnad = 950;
+            }
+
+            Console.WriteLine($"kostnaden för att hyra bilen är {kostnad} kr");
+
+
+        }
+    }
+}
